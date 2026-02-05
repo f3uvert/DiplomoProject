5af42d92b75565c21d5ed472108d109dba68106d
f3uvert <geradaividov@gmail.com>
2026-02-06 00:57:21 +0300

    1

--- main-service/src/main/java/ru/practicum/client/StatsClient.java
+++ main-service/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.client;
 
-import jakarta.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
