218bb670434007678535b0c0ba881e0f2a214a45
f3uvert <geradaividov@gmail.com>
2026-02-05 15:55:44 +0300

    1

--- main-service/src/main/java/ru/practicum/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -35,19 +35,19 @@ public interface EventRepository extends JpaRepository<Event, Long> {
                                 @Param("rangeEnd") LocalDateTime rangeEnd,
                                 Pageable pageable);
 
-    @Query("SELECT e FROM Event e " +
+    @Query(value = "SELECT * FROM events e " +
             "WHERE e.state = 'PUBLISHED' " +
             "AND (:text IS NULL OR :text = '' " +
             "     OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
             "     OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-            "AND (:categories IS NULL OR e.category.id IN :categories) " +
+            "AND (:categories IS NULL OR e.category_id IN (:categories)) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) " +
-            "AND (:onlyAvailable = false " +
-            "     OR e.participantLimit = 0 " +
-            "     OR e.confirmedRequests < e.participantLimit) " +
-            "ORDER BY e.eventDate ASC")
+            "AND (:rangeStart IS NULL OR e.event_date >= :rangeStart) " +
+            "AND (:rangeEnd IS NULL OR e.event_date <= :rangeEnd) " +
+            "AND (:onlyAvailable = false OR e.participant_limit = 0 " +
+            "     OR e.confirmed_requests < e.participant_limit) " +
+            "ORDER BY e.event_date ASC",
+            nativeQuery = true)
     List<Event> findPublicEvents(@Param("text") String text,
                                  @Param("categories") List<Long> categories,
                                  @Param("paid") Boolean paid,
