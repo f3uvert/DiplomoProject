85761d3b7a010ebaa126ba90558eb2ad0555864a
f3uvert <geradaividov@gmail.com>
2026-02-06 04:28:26 +0300

    1

--- main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -2,12 +2,9 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.practicum.client.StatsClient;
 
 @SpringBootApplication
 public class MainServiceApp {
-
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
     }
